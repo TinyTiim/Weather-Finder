383eef962e00a8fb64abc68e1fab6a5862c13dde
unknown <sophia.palomo12@gmail.com>
2023-03-08 23:33:57 -0700

    added my js to try and get the 5-day forcast working

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+
 dayjs.extend(window.dayjs_plugin_utc);
 dayjs.extend(window.dayjs_plugin_timezone);
 
@@ -10,7 +11,11 @@ var date =document.querySelector("#city-date");
 var temp =document.querySelector("#temp");
 var wind =document.querySelector("#wind");
 var humidity=document.querySelector("#humidity");
-
+var cityForecast=document.querySelector("#city-forecast");
+var date1 =document.querySelector("#day1");
+var temp1 =document.querySelector("#temp1");
+var wind1 =document.querySelector("#wind1");
+var hum1 =document.querySelector("#hum1");
 
 
 const history = [];
@@ -21,19 +26,50 @@ button.addEventListener("click",function(){
 fetch("https://api.openweathermap.org/data/2.5/weather?q="+inputValue.value+"&appid="+apiKey+"&units=imperial")
 .then(response => response.json())
 .then(data => {
+
     var nameValue =data['name'];
     var tempValue = data['main']['temp'];
     var windValue = data['wind']['speed'];
     var humidityValue = data['main']['humidity'];
     var dayjs_plugin_timezoneValue= data['sys']['timezone'];
-    
 
-    cityname.innerHTML=nameValue;
-    temp.innerHTML="Temp: "+tempValue+ "°F";
+
+   
+    cityname.innerHTML = nameValue;
+    temp.innerHTML ="Temp: " + tempValue + "°F";
     wind.innerHTML="Wind: "+windValue;
     humidity.innerHTML="Humidity: "+humidityValue;
     timezone = dayjs_plugin_timezoneValue;
+    date.innerHTML = dayjs().format('M/D/YYYY');;
+
+
+  
+
+  
 })
 
 
 })
+
+
+    button.addEventListener("click",function(){
+        fetch("api.openweathermap.org/data/2.5/forecast?q="+inputValue.value+"&appid="+apiKey+"&units=imperial")
+        .then(response => response.json())
+        .then(data => {
+        
+           
+            var tempValue = data['main']['temp'];
+            var windValue = data['wind']['speed'];
+            var humidityValue = data['main']['humidity'];
+            var dayjs_plugin_timezoneValue= data['sys']['timezone'];
+
+
+            
+       
+        temp1.innerHTML ="Temp: " + tempValue + "°F";
+        wind1.innerHTML="Wind: "+windValue;
+        hum1.innerHTML="Humidity: "+humidityValue;
+        timezone = dayjs_plugin_timezoneValue;
+        date1.innerHTML = dayjs().format('M/D/YYYY');;
+        })
+    })
